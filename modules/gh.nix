d69fd9873d819ec4d17f0a75d0a6667ded82bc04
octoclonius <25781800+octoclonius@users.noreply.github.com>
2026-09-19 11:26:15 -0500

    flake update

--- modules/gh.nix
+++ modules/gh.nix
@@ -1,22 +1,32 @@
 _: {
-  flake.modules.homeManager.gh = { config, lib, ... }: {
-    options.my.home.gh.overrides = lib.mkOption {
-      type = lib.types.attrs;
-      default = { };
-    };
+  flake.modules.homeManager.gh =
+    {
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
+    {
+      options.my.home.gh.overrides = lib.mkOption {
+        type = lib.types.attrs;
+        default = { };
+      };
 
-    config = {
-      programs = {
-        gh = lib.recursiveUpdate {
-          enable = true;
-          gitCredentialHelper = {
-            enable = false;
-          };
-          settings = {
-            git_protocol = "ssh";
-          };
-        } config.my.home.gh.overrides;
+      config = {
+        programs = {
+          gh = lib.recursiveUpdate {
+            enable = true;
+            extensions = with pkgs; [
+              gh-stack
+            ];
+            gitCredentialHelper = {
+              enable = false;
+            };
+            settings = {
+              git_protocol = "ssh";
+            };
+          } config.my.home.gh.overrides;
+        };
       };
     };
-  };
 }
